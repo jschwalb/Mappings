b3ace9389f01c7929819c686148234b5a5ce14ac
jschwalb <johannes.schwalb@gmail.com>
2014-04-29 21:04:26 +0200

    Initialized a book entity and implemented the test for it.

--- src/test/java/com/senacor/knowledgetalks/mappingframeworks/mappers/AbstractMapperTest.java
+++ src/test/java/com/senacor/knowledgetalks/mappingframeworks/mappers/AbstractMapperTest.java
@@ -2,12 +2,11 @@ package com.senacor.knowledgetalks.mappingframeworks.mappers;
 
 import com.senacor.knowledgetalks.mappingframeworks.dtos.BookDTO;
 import com.senacor.knowledgetalks.mappingframeworks.dtos.BookTypeDTO;
-import com.senacor.knowledgetalks.mappingframeworks.entities.Book;
+import com.senacor.knowledgetalks.mappingframeworks.entities.*;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.Date;
+import java.util.*;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -22,6 +21,7 @@ public abstract class AbstractMapperTest {
     public void setUp() {
         initializeMappers();
         this.bookDTO = initializeDTO();
+        this.bookEntity = initializeEntity();
     }
 
     protected abstract void initializeMappers();
@@ -43,16 +43,48 @@ public abstract class AbstractMapperTest {
     }
 
     private Book initializeEntity() {
-        //TODO...
+        Book result = new NonFictionBook();
+        result.setTitle("Groovy in Action");
+        result.setPublisher("Manning");
+        result.setReleaseDate(Calendar.getInstance());
 
-        return null;
+        Author author = new Author();
+        author.setLastName("König");
+        author.setFirstName("Dirk");
+        author.setBirthday(new Date());
+        result.setAuthor(author);
+
+        Chapter chapter1 = new Chapter();
+        chapter1.setTitle("One");
+        Chapter chapter2 = new Chapter();
+        chapter2.setTitle("Two");
+        List<Chapter> chapterList = Arrays.asList(chapter1, chapter2);
+        result.setChapters(chapterList);
+
+        return result;
     }
 
     @Test
     public void testMapBookEntity2DTO() {
         BookDTO result = this.mapEntity2DTO(bookEntity);
 
-        //TODO: Assertions...
+        assertNotNull(result);
+        assertEquals(bookEntity.getTitle(), result.getTitle());
+        assertEquals(bookEntity.getPublisher(), result.getPublisher());
+        assertEquals(bookEntity.getReleaseDate().getTime(), result.getReleaseDate());
+
+        if(bookEntity.getAuthor() != null) {
+            Author author = bookEntity.getAuthor();
+            assertEquals(author.getFirstName(), result.getAuthorFirstName());
+            assertEquals(author.getLastName(), result.getAuthorLastName());
+            assertEquals(author.getBirthday(), result.getAuthorBirthday());
+        }
+
+        if(bookEntity.getChapters() != null) {
+            // assertNotNull(result.getChapterTitles());
+            // assertEquals(bookEntity.getChapters().size(), result.getChapterTitles().size());
+            //TODO: Check Content of Chapters...
+        }
     }
 
     public abstract BookDTO mapEntity2DTO(Book bookEntity);
@@ -71,8 +103,8 @@ public abstract class AbstractMapperTest {
         assertEquals(bookDTO.getAuthorLastName(), result.getAuthor().getLastName());
         assertEquals(bookDTO.getAuthorBirthday(), result.getAuthor().getBirthday());
 
-        //assertNotNull(result.getChapters());
-        //assertEquals(bookDTO.getChapterTitles().size(), result.getChapters().size());
+//        assertNotNull(result.getChapters());
+//        assertEquals(bookDTO.getChapterTitles().size(), result.getChapters().size());
         //TODO: Check Content of Chapters...
     }
 
